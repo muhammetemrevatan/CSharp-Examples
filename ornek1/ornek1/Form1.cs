c84f5c47be4ad7b402d787aeeb93134a3f7b7db0
queRy0 <emre.vatan98@hotmail.com>
2019-05-09 22:29:23 +0300

    emre1

--- ornek1/ornek1/Form1.cs
+++ ornek1/ornek1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace ornek1
 {
@@ -15,6 +16,41 @@ namespace ornek1
         public Form1()
         {
             InitializeComponent();
+        }
+        OleDbConnection baglantim = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source="+Application.StartupPath+"\\sözlük.mdb");
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglantim.Open();
+
+            OleDbCommand eklekomutu = new OleDbCommand("Insert into IngTurk(türkce,ingilizce) values('" + textBox1.Text + "','" + textBox2.Text + "')", baglantim);
+            eklekomutu.ExecuteNonQuery();
+            baglantim.Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            baglantim.Open();
+
+            OleDbCommand silkomutu = new OleDbCommand("DELETE FROM IngTurk WHERE ingilizce = '" + textBox1.Text + "' " , baglantim);
+            silkomutu.ExecuteNonQuery();
+            baglantim.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglantim.Open();
+
+            OleDbCommand güncellekomutu = new OleDbCommand("UPDATE IngTurk SET türkce = '"+textBox2.Text+"' WHERE ingilizce like '" + textBox1.Text + "'", baglantim);
+            güncellekomutu.ExecuteNonQuery();
+            baglantim.Close();
         }
     }
 }
